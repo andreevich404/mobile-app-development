2f52f1098fae9b9cc4d66d67bcc5b7a91264c9e6
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2026-09-25 16:11:14 +0700

    feat(lab-1.3): add Components project with EditText, ImageView and Button
    - Добавлен проект Components (задание 3) в корень репозитория
    - Вертикальный LinearLayout с EditText (текст из строкового ресурса), ImageView с иконкой приложения и кнопкой ОК
    - Файлы предыдущей работы (lab-0) удалены из корня ветки

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("jvm") version "2.4.10"
-    application
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-kotlin {
-    jvmToolchain(25)
-}
-
-application {
-    mainClass.set("org.example.MainKt")
-}
-
-tasks.named<JavaExec>("run") {
-    standardInput = System.`in`
-}
-
-tasks.test {
-    useJUnitPlatform()
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
